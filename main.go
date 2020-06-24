ec191d380ab6495c9d7879357a558b3b735dbdd2
mpetavy <mpetavy>
2020-06-24 14:32:27 +0200

    - skipInitialScan - loc corrected

--- main.go
+++ main.go
@@ -17,11 +17,15 @@ type fileitem struct {
 	relname  string
 }
 
-var paths pathlist
+var (
+	paths     pathlist
+	recursive *bool
+)
 
 func init() {
-	common.Init(false, "1.0.0", "2018", "Line of code counter", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, run, 0)
+	common.Init(false, "1.0.1", "2018", "Line of code counter", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, run, 0)
 	flag.Var(&paths, "i", "include directory or file")
+	recursive = flag.Bool("r", false, "recursive file scanning")
 }
 
 func (i *pathlist) String() string {
@@ -40,18 +44,35 @@ func run() error {
 	files := make([]fileitem, 0)
 
 	for _, dir := range paths {
-		recursive := !strings.HasSuffix(dir, string(filepath.Separator))
-		if !recursive {
-			dir = dir[:len(dir)-1]
+		cleanDir := common.CleanPath(dir)
+		mask := ""
+
+		common.Debug("dir: %s", cleanDir)
+		common.Debug("mask: %s", mask)
+
+		if common.ContainsWildcard(cleanDir) {
+			mask = filepath.Base(cleanDir)
+			cleanDir = filepath.Dir(cleanDir)
 		}
 
-		cleanDir := common.CleanPath(dir)
+		err := common.WalkFilepath(cleanDir, *recursive, func(file string) error {
+			var err error
 
-		err := common.WalkFilepath(cleanDir, recursive, func(file string) error {
-			files = append(files, fileitem{
-				fullname: file,
-				relname:  file[len(filepath.Dir(cleanDir))+1:],
-			})
+			b := mask == ""
+			if !b {
+				b, err = common.EqualWildcards(filepath.Base(file), mask)
+				if common.Error(err) {
+					return err
+				}
+			}
+
+			if b {
+				common.Debug("found file: %s", file)
+				files = append(files, fileitem{
+					fullname: file,
+					relname:  file[len(filepath.Dir(cleanDir))+1:],
+				})
+			}
 
 			return nil
 		})
